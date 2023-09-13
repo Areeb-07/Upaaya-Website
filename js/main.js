8bd8c4b32ac36c13d5f323c427bd164a93624b6e
Areeb Asgar <96533217+Areeb-07@users.noreply.github.com>
2023-09-13 17:17:53 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,47 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+$(document).ready(function () {
+
+    $('#navToggle').click(()=>{
+        $('.nav-content').toggleClass('collapse');
+        $('.login-signup').toggleClass('collapse');
+        $('#navToggle').toggleClass('fa-bars');
+        $('#navToggle').toggleClass('fa-times');
+    })
+
+    // owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
+
+
+    // click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+    // AOS Instance
+    AOS.init();
+
+});
